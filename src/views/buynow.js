8213d334c41f14df43536fc91ddf0e392f7cad72
tomaszpankowski <pankowski.email@gmail.com>
2022-01-22 16:00:11 +0100

    routing modified

---
+++ src/views/buynow.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Buynow extends Component{
+    render(){
+        return(        
+            <Container fluid className="buynow-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Buynow;
